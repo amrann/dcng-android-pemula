7588210318cabda56ff2c6854078bef89da5d3e1
mraaan•🍉 <amr.bin.an@gmail.com>
2026-02-02 12:02:33 +0700

    first commit

---
+++ app/src/main/java/com/compose/dicodingpemula/ListTeamsAdapter.kt
@@ -0,0 +1,34 @@
+package com.compose.dicodingpemula
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListTeamsAdapter (private val listTeam: ArrayList<Teams>) : RecyclerView.Adapter<ListTeamsAdapter.ListViewHolder>() {
+
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ): ListViewHolder {
+    val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_team, parent, false)
+    return ListViewHolder(view)
+  }
+
+  override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+    val (name, info, logo) = listTeam[position]
+    holder.imgLogo.setImageResource(logo)
+    holder.tvName.text = name
+    holder.tvInfo.text = info
+  }
+
+  override fun getItemCount(): Int = listTeam.size
+
+  class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val imgLogo: ImageView = itemView.findViewById(R.id.img_item_logo)
+    val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+    val tvInfo: TextView = itemView.findViewById(R.id.tv_item_info)
+  }
+}
---
+++ app/src/main/java/com/compose/dicodingpemula/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.compose.dicodingpemula
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var rvTeams: RecyclerView
+  private val list = ArrayList<Teams>()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_main)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+
+    rvTeams = findViewById(R.id.rv_teams)
+    rvTeams.setHasFixedSize(true)
+
+    list.addAll(getListTeams())
+    showRecyclerList()
+  }
+
+  @SuppressLint("Recycle")
+  private fun getListTeams(): ArrayList<Teams> {
+    val dataName = resources.getStringArray(R.array.arr_club_name)
+    val dataInfo = resources.getStringArray(R.array.arr_club_info)
+    val dataLogo = resources.obtainTypedArray(R.array.arr_club_logo)
+    val listTeams = ArrayList<Teams>()
+    for (i in dataName.indices) {
+      val team = Teams(dataName[i], dataInfo[i], dataLogo.getResourceId(i, -1))
+      listTeams.add(team)
+    }
+    return listTeams
+  }
+
+  private fun showRecyclerList() {
+    rvTeams.layoutManager = LinearLayoutManager(this)
+    val listTeamAdapter = ListTeamsAdapter(list)
+    rvTeams.adapter = listTeamAdapter
+  }
+}
